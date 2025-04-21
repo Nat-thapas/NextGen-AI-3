92790637e68e11b974c3a093d8d10f23a83b2188
Nat-thapas <103213621+Nat-thapas@users.noreply.github.com>
2025-04-21 22:24:45 +0700

    Optimize database config

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,11 +1,22 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 
 import { env } from '$env/dynamic/private';
 
 import * as schema from './schema';
 
+const pool = new Pool({
+	host: env.POSTGRES_HOST,
+	port: +env.POSTGRES_PORT,
+	user: env.POSTGRES_USER,
+	password: env.POSTGRES_PASSWORD,
+	database: env.POSTGRES_DB,
+	max: 10,
+	ssl: false
+});
+
 export const db = drizzle({
-	connection: `postgres://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@${env.POSTGRES_HOST}:${env.POSTGRES_PORT}/${env.POSTGRES_DB}`,
+	client: pool,
 	casing: 'snake_case',
 	schema,
 	logger: env.NODE_ENV.toLowerCase() !== 'production'
